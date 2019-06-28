2518f6575149c30483d0951e1fa13a6cac64fb6f
Mike Z <mzapata2991@gmail.com>
2019-06-27 20:54:52 -0500

    testing chat with heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// const http = require('http').Server(app);
+const io = require('socket.io')(app);
 
 var PORT = process.env.PORT || 8080;
 
