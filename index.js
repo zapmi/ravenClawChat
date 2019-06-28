1e2e903c732d4449b7a61c01d98f664482fc963c
Mike Z <mzapata2991@gmail.com>
2019-06-27 20:25:45 -0500

    removed local host from code for heroku

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ io.sockets.on('connection', function (socket) {
 
 
 
-const server = http.listen(8080, function () {
-    console.log('listening on *:8080');
+const server = http.listen(PORT, function () {
+    // console.log('listening on *:8080');
 });
