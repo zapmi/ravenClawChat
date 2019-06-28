19a4a9e4113e86119b3d8ed799e63bb86afb7c00
Mike Z <mzapata2991@gmail.com>
2019-06-27 20:49:16 -0500

    reverted back to local chat changes

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-// const io = require('socket.io')(http);
-const io = socketIO(server);
+const io = require('socket.io')(http);
 
-const server = express()
-.use((req, res) => res.sendFile(chat.ejs))
-.listen(PORT, () => console.log('Listening on ${ PORT }'));
+var PORT = process.env.PORT || 8080;
 
-// app.get('/', function (req, res) {
-//     res.render('chat.ejs');
-// });
-
-
+app.get('/', function (req, res) {
+    res.render('chat.ejs');
+});
 
 io.sockets.on('connection', function (socket) {
     socket.on('username', function (username) {
@@ -24,13 +19,13 @@ io.sockets.on('connection', function (socket) {
     })
 
     socket.on('chat_message', function (message) {
-        
+        setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
         io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
     });
 });
 
 
 
-// const server = http.listen(PORT, function () {
-//     console.log('listening on *:8080');
-// });
+const server = http.listen(PORT, function () {
+    console.log('listening on *:8080');
+});
